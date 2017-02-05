7387f038ceab63bc055720ff6de8918d45050464
Emile <emile.app@gmail.com>
2017-02-05 13:40:28 +0200

    added navmesh & navmeshagent

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	public bool goToDestination = false;
+	public GameObject target = null;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (goToDestination && target != null) {
+			agent.SetDestination(target.transform.position);
+		}
+	}
+}
